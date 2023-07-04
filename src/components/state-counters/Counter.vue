abb15321807377d0a61ad3e74f47a0ea959b6b6d
Elian Van Cutsem <hello@elian.codes>
2023-07-04 11:23:29 +0200

    feat: add basic example setup

---
+++ src/components/state-counters/Counter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="flex items-center gap-8 justify-center">
+    <button @click="count++">+</button>
+    <p>Vue count is: {{ count }}</p>
+    <button @click="count--">-</button>
+  </div>
+</template>
